bd6c4fdeaeff939ec85dab91de9632287b37032b
jbravoh <jbravoharriott@gmail.com>
2022-03-15 08:48:26 +0000

    Setting up Firebase

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+// initialize firebaseConfig and connect to firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC2fuxLwmEibu0SAK5olnnCNqrAP-4_VdA",
+    authDomain: "fir-9-project-89570.firebaseapp.com",
+    projectId: "fir-9-project-89570",
+    storageBucket: "fir-9-project-89570.appspot.com",
+    messagingSenderId: "134763722083",
+    appId: "1:134763722083:web:1ed6d4112585149b398f56"
+};
+
+initializeApp(firebaseConfig) 
